037610a127c87b645aea00dd9c1930e33b040064
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2020-12-30 11:31:26 +0330

    Update args.js

--- args.js
+++ args.js
@@ -7,3 +7,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script>
+        /*
+            var a =function test(){
+    
+            }
+            alert('asasasa')
+            (
+                function(){
+    
+                }()
+            )
+            به صورت خود کار فرا خوانی می شود*/
+
+        function test(/*a,b*/) {
+
+            var d = 0;
+
+            alert(arguments.length);//تعداد مغادیر
+            alert(arguments[0]);//مغدار اول
+            for (var a in arguments) {
+                console.log(arguments[a]);
+                d += arguments[a];
+            }
+            alert(d)
+        }
+        test(10, 20, 30, 40, 50);
+
+
+
+
+
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
